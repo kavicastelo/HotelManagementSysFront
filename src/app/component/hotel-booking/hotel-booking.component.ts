3082753a11595b3b305cb06f5aba51238ea85e13
KAVI <kavindu123450@gmail.com>
2023-01-24 22:18:59 +0530

    5

---
+++ src/app/component/hotel-booking/hotel-booking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-booking',
+  templateUrl: './hotel-booking.component.html',
+  styleUrls: ['./hotel-booking.component.scss']
+})
+export class HotelBookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
