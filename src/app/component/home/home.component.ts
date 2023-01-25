ef9443d29617280de62c1179f723ada87f984d76
KAVI <kavindu123450@gmail.com>
2023-01-26 01:23:29 +0530

    7

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -36,5 +37,6 @@ export class HomeComponent implements OnInit {
       this.activeAvailableBtn = true;
     }
   }
+
 
 }
