aec45ed579f05a5b409872e4a82c6ecbc0bf62ce
KAVI <kavindu123450@gmail.com>
2023-01-29 01:02:27 +0530

    14

--- src/app/component/hotel-booking/hotel-booking.component.ts
+++ src/app/component/hotel-booking/hotel-booking.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelBookingComponent implements OnInit {
 
+  ArrivalMinDate = new Date();
+  public DepartureMinDate = new Date();
+  public ArrivalMaxDate = new Date();
+
+  public selectCustomer='';
+  public selectChildren='';
+
+  public arrivalValue = localStorage.getItem("arrivalDate");
+  public arrivalValueDate = new Date(''+this.arrivalValue);
+  public departureValue = localStorage.getItem("departureDate");
+  public departureValueDate = new Date(''+this.departureValue);
+  public adultValue = localStorage.getItem("customers");
+  public childrenValue = localStorage.getItem("children");
+
   constructor() { }
 
   ngOnInit(): void {
+
+    console.log(this.arrivalValue);
+    console.log(this.departureValue);
+    console.log(this.adultValue);
+    console.log(this.childrenValue);
   }
 
 }
