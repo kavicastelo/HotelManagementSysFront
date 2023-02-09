532f6f1bba1ea72abc317b4ad54141a781ebd27c
KAVI <kavindu123450@gmail.com>
2023-02-09 13:59:17 +0530

    33

---
+++ src/app/component/spa/spa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spa',
+  templateUrl: './spa.component.html',
+  styleUrls: ['./spa.component.scss']
+})
+export class SpaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
